eeff22a678c70425597ac2496b6842cc57202cb4
Noob <ozbm1000@gmail.com>
2023-01-13 13:14:32 +0200

    "e"

---
+++ asembler.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //get input
+
+    //open file
+    //parse it 
+    //pre process
+    //start parsing it to binary/././././../././
+    //push back to file
+}
+
+
