d1825fbe2244d5445720acb1cd98528d246dcf7d
Noob <ozbm1000@gmail.com>
2023-01-14 16:42:01 +0200

    yes

--- testthingy.c
+++ testthingy.c
@@ -9,7 +9,7 @@ int main()
     fp1=fopen("f1","r");
     fp2=fopen("f2","w");
     
-    while(fgets(line, SIZE,fp1!=NULL))
+    while(fgets(line, SIZE,fp1)!=NULL)
     {
         if(strcmp(line,"aaaa")!=0)
         {
@@ -17,7 +17,7 @@ int main()
         }
     }
     remove(fp1);
-    //remove(file pointer) removes a file, 0 if worked
+    /*remove(file pointer) removes a file, 0 if worked*/
     fclose(fp2);
 
     return 1;
