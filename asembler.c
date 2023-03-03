22009cb1c2153413d214013a228835daabaf70fb
Noob <ozbm1000@gmail.com>
2023-03-03 12:24:07 +0200

    pushing for remote fixing

--- asembler.c
+++ asembler.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <struct.h>
+#include <macroDecoder.c>
 
 
-int main()
+int main(int argc, char *argv[])
 {
+    int i=0;
+    FileList macroNode,tempNode,inputFilesHead,macroFilesHead;
+    macroNode=helpers.nuller(macroNode);
+    tempNode=helpers.nuller(tempNode);
+    inputFilesHead=helpers.nuller(inputFilesHead);
+    macroFilesHead=helpers.nuller(macroFilesHead);
+    
+    if(argc<=1)
+    {
+        printf("please input file names");
+        return 1; 
+    }
+    inputFilesHead=stringToFiles(argc,argv);
+    tempNode=inputFilesHead;
+    macroFilesHead=macroNode;
+    for (i=1;i<argc-1;i++)
+    {
+        macroNode.p=macroDecoder(tempNode.p);
+        if(macroNode.p==NULL)/**assuming that the Macro decoder has found some error and finished early, after printing them**/
+        {
+            return 1;
+        }
+        tempNode=*tempNode.next;
+        macroNode=*macroNode.next;
+    }
+
+    helper.closeFileList(inputFilesHead);
     //get input
 
     //open file
@@ -10,6 +40,6 @@ int main()
     //pre process
     //start parsing it to binary/././././../././
     //push back to file
+    return 1;
 }
-
 
--- testthingy.c
+++ testthingy.c
@@ -4,24 +4,23 @@
 
 int main()
 {
-    FILE *fp1,*fp2;
-    char line[SIZE];
-    fp1=fopen("f1","r");
-    fp2=fopen("f2","w");
-    
-    while(fgets(line, SIZE,fp1)!=NULL)
-    {
-        if(strcmp(line,"aaaa")!=0)
-        {
-            printf("%s",line);
-            fputs(line,fp2);
-        }
-    }
-    remove("f1");
-    /*remove(file pointer) removes a file, 0 if worked*/
-    fclose(fp2);
+//     FILE *fp1,*fp2;
+//     char line[SIZE];
+//     fp1=fopen("f1","r");
+//     fp2=fopen("f2","w");
+    printf("yeeeeeee");    
+//     while(fgets(line, SIZE,fp1)!=NULL)
+//     {
+//         if(strcmp(line,"aaaa")!=0)
+//         {
+//             printf("%s",line);
+//             fputs(line,fp2);
+//         }
+//     }
+//     remove("f1");
+//     /*remove(file pointer) removes a file, 0 if worked*/
+//     fclose(fp2);
 
     return 1;
 }
-
 
