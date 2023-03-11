9ca4f933ef99445dbaee2634956cb896d28acd6f
Noob <ozbm1000@gmail.com>
2023-03-11 19:24:50 +0200

    yyyyyyyy

--- asembler.c
+++ asembler.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     for (i=1;i<=argc-1;i++)
     {
         macroDecoder(tempNode->file,macroNode);
-        if(macroNode.file==NULL)/*assuming that the Macro decoder has found some error and finished early, after printing them*/
+        if(macroNode->file==NULL)/*assuming that the Macro decoder has found some error and finished early, after printing them*/
         {
             return 1;
         }
