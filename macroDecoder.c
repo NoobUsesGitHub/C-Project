d51b367f46a3820c0e413c3f67ee8d2a1242da67
Noob <ozbm1000@gmail.com>
2023-03-11 20:39:23 +0200

    I hate string operations

--- macroDecoder.c
+++ macroDecoder.c
@@ -13,6 +13,7 @@ FileList* macroDecoder(FILE* fp,char* fileName)
     char strNewName[strlen(fileName)];
     strcpy(strNewName,fileName);
     strNewName[strlen(strNewName)-1]='m';
+    macroNode->fileName=(char*)malloc(strlen(strNewName)*sizeof(char));
     strcpy(macroNode->fileName,strNewName);/*to change*/
     macroNode->file=fopen(strNewName,"w");
     while (fgets(str,MAXLINESIZE, fp)) 
