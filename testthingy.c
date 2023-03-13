38e9de652ca6732f32b599634ece6e7543ca3689
Noob <ozbm1000@gmail.com>
2023-03-13 22:51:42 +0200

    started checking mcrs

--- testthingy.c
+++ testthingy.c
@@ -4,12 +4,21 @@
 
 int main ()
 {
-  char str[] ="This     a\tsample\tstring";
+  char str[] ="mcr aaaaa This     a\tsample\tstring endmcr aadsad";
   char * pch;
   printf ("Splitting string \"%s\" into tokens:\n",str);
   pch = strtok (str,"    \t \f \r");
   while (pch != NULL)
   {
+    if(strcmp(pch,"mcr")==1)
+    {
+        printf("mcr started");
+    }
+    
+    if(strcmp(pch,"endmcr")==1)
+    {
+        printf("mcr ended");
+    }
     printf ("%s\n",pch);
     pch = strtok (NULL, " ,.-");
   }
