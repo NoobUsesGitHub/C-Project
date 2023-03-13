4eb82455660deeb926b33ffa96bdf1ce6c520a6f
Noob <ozbm1000@gmail.com>
2023-03-13 23:30:15 +0200

    checking for ;

--- testthingy.c
+++ testthingy.c
@@ -8,12 +8,21 @@ int main ()
   char * pch;
   printf ("Splitting string \"%s\" into tokens:\n",str);
   pch = strtok (str,"    \t \f \r");
+  int i=0;
   while (pch != NULL)
   {
+    if(i==0)
+    {
+        
+    /*check for;*/
+        printf ("checking first note %c\n",pch[0]);
+
+    }
     if(strcmp(pch,"mcr")==1)
     {
         printf("mcr started");
     }
+
     
     if(strcmp(pch,"endmcr")==1)
     {
@@ -21,6 +30,7 @@ int main ()
     }
     printf ("%s\n",pch);
     pch = strtok (NULL, " ,.-");
+    i++;
   }
   return 0;
 }
