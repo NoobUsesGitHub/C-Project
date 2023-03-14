2ab8f699258a2ab33c10902d64d3568027be5b16
Noob <ozbm1000@gmail.com>
2023-03-14 23:59:33 +0200

    skipper

--- testthingy.c
+++ testthingy.c
@@ -14,6 +14,7 @@ int main ()
   char * pch;
   /*printf ("Splitting string \"%s\" into tokens:\n",str);*/
   int i=0;
+  int skip=0;
   char *str[85];
   while(fgets(str,85,f)){
   pch = strtok (str,"    \t \f \r");
@@ -24,20 +25,26 @@ int main ()
     {
         if(pch[0]==';')
             printf ("first note is ; %c\n",pch[0]);
+        skip=1;
     }
 
     if(strcmp(pch,"mcr")==0)
     {
         printf("mcr started");
+        skip=1;
     }
 
     if(strcmp(pch,"endmcr")==0)
     {
         printf("mcr ended");
+        skip=1;
     }
-    printf ("%s\n",pch);
+    
+    if(skip1=1)
+      printf ("%s\n",pch);
     pch = strtok (NULL, "    \t \f \r");
     i++;
+    skip=0;
   }}
   return 0;
 }
