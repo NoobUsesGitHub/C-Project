e5c0552d88e41578be1f1152c58175bcb3f20b09
Noob <ozbm1000@gmail.com>
2023-03-24 00:58:46 +0200

    D:

--- testthingy.c
+++ testthingy.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-void removeRedundantSpaces(char* str) {
+
+
+void removeRedundantSpaces(char *str)
+{
     int i, j, len;
-    len = strlen(str);
+    len = strlen(str); /*to change*/
     j = 0;
-    for (i = 0; i < len; i++) {
-        if (str[i] != ' ' && str[i] != '\t') {
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] != ' ' && str[i] != '\t')
+        {
             str[j] = str[i];
             j++;
-        } else if (i > 0 && str[i-1] != ' ' && str[i-1] != '\t') {
+        }
+        else if (i > 0 && str[i - 1] != ' ' && str[i - 1] != '\t')
+        {
             str[j] = ' ';
             j++;
         }
+        else if (i > 0 && str[i] != ',' && str[i - 1] != ' ' && str[i - 1] != '\t')
+        {
+            str[j] = ',';
+            j++;
+        }
     }
+    if(str[j-1]==' '||str[j-1]=='\t')
+        j--;
     str[j] = '\0';
 }
 
