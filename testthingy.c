c5b7b7a21cbf06c261835d7a312dcc802d031dc4
Noob <ozbm1000@gmail.com>
2023-03-24 01:24:36 +0200

    das

--- testthingy.c
+++ testthingy.c
@@ -12,12 +12,6 @@ void removeRedundantSpaces(char *str)
     {
         if (isspace(str[i])==0)
         {
-          if(i>0 && str[i]==',' && isspace(str[j])==1)
-          {
-            str[j]=',';
-            j++;
-            continue;  
-          }
             str[j] = str[i];
             j++;
         }
